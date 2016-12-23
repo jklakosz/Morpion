9b1ffa9f185b0f9099a75bb6ff7d6ea91c4e39b8
ryukk <j.klakosz@gmail.com>
2016-12-23 01:29:48 +0100

    Initital commit

--- src/main/java/fr/ryukk/morpion/Morpion.java
+++ src/main/java/fr/ryukk/morpion/Morpion.java
@@ -2,13 +2,11 @@ package fr.ryukk.morpion;
 
 public class Morpion {
 
+    public static Morpion instance;
+
     public static void main(String[] args) {
 
-
-
-
-
-
+        instance = new Morpion();
 
     }
 
